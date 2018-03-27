eb595f2757666a679426d12df04b33379992396a
Samuel Plumppu <greenheartmusic@gmail.com>
2018-03-27 20:45:54 +0200

    fix(typo): One typo less!

--- js/rooms/intro/start.js
+++ js/rooms/intro/start.js
@@ -8,5 +8,5 @@ window.rooms.push({
         You're on your way to your best friend Kevin's place. He tried calling you three times earlier today and eventually left a message.<br>
     </p>
     <p><i>"I can't believe this is happening... I have something important to tell you. Could you come over?"</i></p>
-    <p>So here you are on the street, leading <b>north</b> to your his apartment.</p>`
+    <p>So here you are on the street, leading <b>north</b> to his apartment.</p>`
 })
