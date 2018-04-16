8121c822439780216b77f59060e4a7ecc23058e1
Samuel Plumppu <greenheartmusic@gmail.com>
2018-04-16 18:14:37 +0200

    feature(story): Add ideas for how to expand the story told in the window

--- js/rooms/apartment/window.js
+++ js/rooms/apartment/window.js
@@ -7,6 +7,19 @@ window.rooms.push({
     items: [
         // IDEA: Add 2-3 notes here?
         // This could be used to describe how Kevin was trying to connect his experiences, to understand what was happening.
+
+        // IDEA:
+        // - Kevin signed a contract that he can't get out of.
+            // 	- Either you, as the player help him, or you try save the world, or you try to do both!
+            // 	- Because of the memory issues, he might not even remember the contract.
+
+        // - In these notes, he explain how he at first was excited about the work. But at a later time, he lost that enthusiasm.
+
+        // - Kevin witnessed another employee who had gone through the whole process - and he was scared.
+        	// - The results on the weren't favourable.
+        	// - Before he lost his mind, he choose to contact someone.
+        	// 	- Maybe they get involved in the story and play a part.
+        	// - The company might have picked up on this, trying to reach out to Kevin.
     ],
     // IDEA: Maybe write something about the outside, seen through the window?
     description: `<p>The inside of the window has several notes attached. They seem to be organized, with lines drawn on the glass connecting them.</p>
