2ad809053fae5fb04d150db6072e00e5cabf0624
Samuel Plumppu <greenheartmusic@gmail.com>
2018-04-26 17:35:51 +0200

    fix(minimap/compass): Update `player.wayBack` after moving the player.
    This ensures that the direction is correct.
    
    Also unset the `wayBack` if the direction isn't possible to go back to.

--- js/player.js
+++ js/player.js
@@ -153,13 +153,13 @@ class Player {
             // Room.playerCanLeave() should return true if a player can leave, otherwise a string with the reason.
             const playerCanLeave = this.currentRoom.playerCanLeave ? this.currentRoom.playerCanLeave(direction) : true
             if (playerCanLeave === true) {
+                this.moveTo(this.currentRoom.connections[direction])
+
                 // Add the way back to the previous room, if the room is connected.
                 // Since some rooms only offer one-way connections, `wayBack` only make sense in some cases.
                 const wayBack = Helpers.getOppositeDirection(direction)
-                if (this.currentRoom.connections[wayBack]) {
-                    this.wayBack = wayBack
-                }
-                this.moveTo(this.currentRoom.connections[direction])
+                this.wayBack = this.currentRoom.connections[wayBack] ? wayBack : ''
+                this.updateMinimap()
             } else {
                 // Show the reason why player can't leave.
                 this.game.status(playerCanLeave)
