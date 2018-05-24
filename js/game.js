b4cc37866faafcf939213729b845a785a0a939f0
Samuel Plumppu <greenheartmusic@gmail.com>
2018-05-24 14:28:55 +0200

    fix(keyboard): Use `event.key` to support multiple keyboard layouts.
    Turns out the latest commit removing `event.key` because of deprecation wasn't entierly correct - only `event.keyCode` is to be deprecated, according to https://mdn.io/event.keyCode

--- js/game.js
+++ js/game.js
@@ -374,7 +374,7 @@ class Game {
         })
 
         this.ui.userInput.addEventListener('keydown', event => {
-            switch (event.code) {
+            switch (event.key) {
                 case 'Enter':
                     // Only allow enter to be pressed once at a time.
                     if (enterKeyIsDown) return
@@ -402,7 +402,7 @@ class Game {
         })
 
         this.ui.userInput.addEventListener('keyup', event => {
-            if (event.code === 'Enter') enterKeyIsDown = false
+            if (event.key === 'Enter') enterKeyIsDown = false
         })
         this.ui.userInput.focus()
     }
